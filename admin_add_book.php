f9203d1e1a1e121b49b29100ed7bb0d1a28f09bd
hieunoobpro <hieudsgh@gmail.com>
2023-05-19 09:42:41 +0700

    change

--- admin_add_book.php
+++ admin_add_book.php
@@ -1,97 +1,22 @@
 <?php
-    include('./layout/admin_header.php')
-?>
-         <!-- Page Content  -->
-         <div id="content-page" class="content-page">
-            <div class="container-fluid">
-               <div class="row">
-                  <div class="col-sm-12">
-                     <div class="iq-card">
-                        <div class="iq-card-header d-flex justify-content-between">
-                           <div class="iq-header-title">
-                              <h4 class="card-title">Add Books</h4>
-                           </div>
-                        </div>
-                        <div class="iq-card-body">
-                           <form action="./admin_handle/admin_add_book.php" method="POST">
-                              <div class="form-group">
-                                 <label>Book Name:</label>
-                                 <input type="text" name="name" class="form-control">
-                              </div>
-                              <div class="form-group">
-                                 <label>Book Category:</label>
-                                 <select class="form-control" name="category" id="exampleFormControlSelect1">
-                                    <option selected="" disabled="">Book Category</option>
-                                    <?php
-                                    $sql ="SELECT `ctg_id`, `ctg_name`, `ctg_description` FROM `category` WHERE 1";
-                                    $ketqua = mysqli_query($conn,$sql);
-                                    if($ketqua->num_rows>0){
-                                       while($ctg =$ketqua->fetch_assoc()){
-                                          echo "<option value=".$ctg['ctg_id'].">".$ctg['ctg_name']."</option>";
-                                       }
-                                    }
-                                    ?>
-                                 </select>
-                              </div>
-                              <div class="form-group">
-                                 <label>Book Author:</label>
-                                 <select class="form-control" name="author" id="exampleFormControlSelect2">
-                                    <option selected="" disabled="">Book Author</option>
-                                    <?php
-                                    $sql ="SELECT * FROM `author` WHERE 1";
-                                    $ketqua1 = mysqli_query($conn,$sql);
-                                    if($ketqua1->num_rows>0){
-                                       while($author =$ketqua1->fetch_assoc()){
-                                          echo "<option value=".$author['author_id'].">".$author['author_name']."</option>";
-                                       }
-                                    }
-                                    ?>
-                                 </select>
-                              </div>
-                              <div class="form-group">
-                                 <label>Book Publisher:</label>
-                                 <select class="form-control" name="publisher" id="exampleFormControlSelect2">
-                                    <option selected="" disabled="">Book Publisher</option>
-                                    <?php
-                                    $sql ="SELECT * FROM `publisher` WHERE 1";
-                                    $ketqua2 = mysqli_query($conn,$sql);
-                                    if($ketqua2->num_rows>0){
-                                       while($publisher =$ketqua2->fetch_assoc()){
-                                          echo "<option value=".$publisher['publisher_id'].">".$publisher['publisher_name']."</option>";
-                                       }
-                                    }
-                                    ?>
-                                 </select>
-                              </div>
-                              <div class="form-group">
-                                 <label>Book Image:</label>
-                                 <div class="custom-file">
-                                    <input type="file" name="img" accept="image/png, image/jpeg">
-                                 </div>
-                              </div>
-                              <div class="form-group">
-                                 <label>Book Price:</label>
-                                 <input type="text" name="price" class="form-control">
-                              </div>
-                              <div class="form-group">
-                                 <label>Book in Stock:</label>
-                                 <input type="text" name="stock" class="form-control">
-                              </div>
-                              <div class="form-group">
-                                 <label>Book Description:</label>
-                                 <textarea class="form-control" name="descrip" rows="4"></textarea>
-                              </div>
-                              <button type="submit" class="btn btn-primary">Submit</button>
-                              <button type="reset" class="btn btn-danger">Reset</button>
-                           </form>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-      <!-- Wrapper END -->
-<?php
-    include('./layout/admin_footer.php')
+require_once('../db/connect.php');
+$name =$_POST['name'];
+$author =$_POST['author'];
+$category =$_POST['category'];
+$publisher =$_POST['publisher'];
+$img =$_POST['img'];
+$price =$_POST['price'];
+$stock =$_POST['stock'];
+$descrip =$_POST['descrip'];
+$date = date("Y/m/d");
+$sql = "INSERT INTO `product`( `prd_name`, `prd_in_stock`, `prd_in_order`, `prd_price`, `prd_img`, `prd_release`, `publisher_id`, `author_id`, `ctg_id`, `prd_description`) 
+VALUES ('".$name."','".$stock."',0,'".$price."','".$img."','".$date."','".$publisher."','".$author."','".$category."','".$descrip."')";
+
+if($conn->query($sql)){
+    header('Location: ../admin_books.php' );
+}else{
+    echo "error: ".$conn->error;
+}
+
+
 ?>
--- admin_add_publisher.php
+++ admin_add_publisher.php
@@ -1,35 +1,11 @@
 <?php
-    include('./layout/admin_header.php')
-?>
-         <!-- Page Content  -->
-         <div id="content-page" class="content-page">
-            <div class="container-fluid">
-               <div class="row">
-                  <div class="col-sm-12">
-                     <div class="iq-card">
-                        <div class="iq-card-header d-flex justify-content-between">
-                           <div class="iq-header-title">
-                              <h4 class="card-title">Add Publisher</h4>
-                           </div>
-                           
-                        </div>
-                        <div class="iq-card-body">
-                           <form action="./admin_handle/admin_add_publisher.php" method="POST">
-                              <div class="form-group">
-                                 <label>Publisher Name:</label>
-                                 <input type="text" name="name" class="form-control" >
-                              </div>
-                              <button type="submit" class="btn btn-primary">Submit</button>
-                              <button type="reset" class="btn btn-danger">Reset</button>
-                           </form>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-      <!-- Wrapper END -->
-<?php
-    include('./layout/admin_footer.php')
+require_once('../db/connect.php');
+$publisher_name =$_POST['name'];
+
+$sql ="INSERT INTO `publisher`(`publisher_name`) VALUES ('".$publisher_name."')";
+if($conn->query($sql)){
+    header('Location: ../admin_publisher.php' );
+}else{
+    echo "error: ".$conn->error;
+}
 ?>
